2bba6adabcba49cd4a420eb047029c748f1fca5a
sfc-gh-samitjain <samit.jain@snowflake.com>
2024-02-27 10:05:12 -0500

    firstcommit

---
+++ app.py
@@ -0,0 +1,15 @@
+from snowflake.snowpark.session import Session 
+from snowflake.snowpark import DataFrame 
+from snowflake.snowpark.functions import col
+import json
+
+def hello(session: Session) -> DataFrame:
+    df = session.table("sample_db.public.customers_sample")
+    #df = df.groupBy("STATE").count()
+    return df
+
+# For local debugging
+#if __name__ == "__main__":
+#    session = Session.builder.configs(json.load(
+#      open("/change_your_path/snowflake_connection.json"))).create()
+#    print (hello (session).show())
