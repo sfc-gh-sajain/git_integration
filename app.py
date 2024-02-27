788cdf44cc190b714d03dd7616b8c4da8c694feb
sfc-gh-samitjain <samit.jain@snowflake.com>
2024-02-27 14:29:24 -0500

    5commit

--- app.py
+++ app.py
@@ -5,12 +5,11 @@ import json
 
 def hello(session: Session) -> DataFrame:
     df = session.table("sample_db.public.customers_sample")
-    #df = df.groupBy("STATE").count()
-    df = df.filter(col("STATE") == 'MS')
+#    df = df.filter(col("STATE") == 'MS')
     return df
 
 # For local debugging
-#if __name__ == "__main__":
-#    session = Session.builder.configs(json.load(
-#      open("/change_your_path/snowflake_connection.json"))).create()
-#    print (hello (session).show())
+if __name__ == "__main__":
+    session = Session.builder.configs(json.load(
+      open("/change_your_path/snowflake_connection.json"))).create()
+    print (hello (session).show())
